77cc7bd55131e0ee341a0135e6b6ab756dbe1f25
nomanHasan <nomanbinhussein@gmail.com>
2018-03-22 01:18:25 +0600

    Data Add done

--- RandomPersonElasticsearch/Program.cs
+++ RandomPersonElasticsearch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Elasticsearch.Net;
 using System.Diagnostics;
@@ -41,11 +42,29 @@ namespace RandomPersonElasticsearch
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
-
-            bool firstLine = true;
             
+            int bulkLimit = 1000;
+            int counter = 0;
+            int id = 0;
+
+
+            var people = new List<object>
+            {
+                new { index = new { _index = "people", _type = "_doc" }}
+            };
+
+            var person = new Person
+            {
+                FirstName = "Kuddus",
+                LastName = "Miah"
+            };
+
+            var indexCreateResponse = lowLevelClient.Index<BytesResponse>("people", "_doc", PostData.Serializable(person));
+
+
             using (StreamReader reader = new StreamReader(csv_filename))
             {
+                reader.ReadLine();
                 while (true)
                 {
                     string line = reader.ReadLine();
@@ -53,38 +72,47 @@ namespace RandomPersonElasticsearch
                     {
                         break;
                     }
-
-                    if(firstLine)
+                    
+                    var values = line.Split(",");
+                    
+                    if (counter >= bulkLimit)
                     {
-                        firstLine = false;
-                        continue;
+                        var indexResponse = lowLevelClient.Bulk<StringResponse>(PostData.MultiJson(people));
+                        counter = 0;
+                        people = new List<object>
+                        {
+                           
+                        };
+                        Console.WriteLine("Bulk Insert");
+                    }
+                    else
+                    {
+                        people.Add(new { index = new { _index = "people", _type = "_doc", _id = $"{id}" } });
+                      
+                        people.Add(
+                        new
+                        {
+                            FirstName = values[0],
+                            LastName = values[1],
+                            UserName = values[2],
+                            SSC_Grade = values[3],
+                            HSC_Grade = values[4],
+                            Bachelor_Grade = values[5],
+                            Age = Int32.Parse(values[6]),
+                            Gender = values[7],
+                            Email = values[8],
+                            DateOfBirth = DateTime.Parse(values[9]),
+                            Street = values[10],
+                            Suite = values[11],
+                            City = values[12],
+                            ZipCode = values[13],
+                        });
+                        counter++;
                     }
 
-                    var values = line.Split(",");
+                    id++;
 
-                    var person = new Person
-                    {
-                        FirstName = values[0],
-                        LastName = values[1],
-                        UserName = values[2],
-                        SSC_Grade = values[3],
-                        HSC_Grade = values[4],
-                        Bachelor_Grade = values[5],
-                        Age = Int32.Parse(values[6]),
-                        Gender = values[7],
-                        Email = values[8],
-                        DateOfBirth = DateTime.Parse(values[9]),
-                        Street = values[10],
-                        Suite = values[11],
-                        City = values[12],
-                        ZipCode = values[13],
-                    };
-
-                    var indexResponse = lowLevelClient.Index<BytesResponse>("person", "_doc",
-                     PostData.Serializable(person)
-                    );
-
-                    Console.WriteLine(line);
+                    //Console.WriteLine(line);
                 }
             }
 
